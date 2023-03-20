cf53d83ea372028ad2193f21ee228e0d711ab90a
nguyenhuudung201 <115616484+nguyenhuudung201@users.noreply.github.com>
2023-03-20 10:40:28 +0700

    up java code

---
+++ src/com/class1/Main.java
@@ -0,0 +1,22 @@
+package com.class1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Create objects
+        Person person1 = new Person();
+
+        person1.inputData();
+        person1.display();
+
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Please enter name: ");
+//        String name = input.nextLine();
+//
+//        System.out.print("Please enter mark: ");
+//        int mark = input.nextInt();
+//
+//        System.out.println("Hello " + name + ", your mark is = " + mark);
+
+    }
+}
